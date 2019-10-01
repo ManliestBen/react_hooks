169ddc9a52949f2d51ab3c345ca792a2bb3cc5ed
ManliestBen <benjamintmanley@gmail.com>
2019-10-01 13:11:56 -0500

    clean up code

--- src/components/BatteryHookContainer/BatteryHookContainer.jsx
+++ src/components/BatteryHookContainer/BatteryHookContainer.jsx
@@ -1,13 +1,32 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Battery from '../Battery/Battery';
+import {register, unregister} from '../../utils/battery';
+
 
 function BatteryHookContainer() {
-    const arr = useState(.55);
-    const level = arr[0];
-    const setLevel = arr[1];
+    const [batteryData, setBatteryData] = useState({
+        level: .55,
+        charging: true
+    });
+
+    useEffect(() => {
+        register(updateBattery);
+        return () => {
+            unregister(updateBattery);
+        }
+    });
+    
+    function updateBattery(data) {
+        setBatteryData(data);
+    }
     return (
-    <Battery level={level} />
-    )
+        <>
+          <Battery 
+          level={batteryData.level}
+          charging={batteryData.charging}
+          />
+        </>
+    );
 }
 
 export default BatteryHookContainer;
